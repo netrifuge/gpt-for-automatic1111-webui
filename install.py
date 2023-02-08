7980ba2f09c37615dacb1e8c23a0d476b177bc49
pancoaster <92821098+pancoaster@users.noreply.github.com>
2023-02-08 13:10:05 -0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+from launch import run, git_clone, repo_dir
+
+name = "Prompts from GPT"
+req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+print(f"loading {name} reqs from {req_file}")
+run(f'"{sys.executable}" -m pip install -r "{req_file}"', f"Checking {name} requirements.",
+    f"Couldn't install {name} requirements.")
